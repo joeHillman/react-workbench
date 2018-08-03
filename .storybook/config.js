0a0afc868c33130ffa4060830986f7e5f9e9a74a
Joe Hillman <joerhillman@gmail.com>
2018-08-03 19:13:50 -0400

    Storybook addons for customizations

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,14 @@
 import { configure } from "@storybook/react";
+import { setOptions } from '@storybook/addon-options';
 import "./base.css";
 import "../src/stylesheets/styles";
 
 const req = require.context("../src/stories/", true, /\.js$/)
+
+setOptions({
+  name: "Joe Hillman - React Work",
+  url: "https://joehillman.github.io/react-workbench/"
+});
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
