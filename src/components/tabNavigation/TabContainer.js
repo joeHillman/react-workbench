079664e3c16877321092ba081354f25fb38c6b28
Joe Hillman <joerhillman@gmail.com>
2018-08-08 14:46:55 -0400

    Cleanup render method of tab container component.

--- src/components/tabNavigation/TabContainer.js
+++ src/components/tabNavigation/TabContainer.js
@@ -12,28 +12,31 @@ class TabContainer extends Component {
     }
   }
 
+  thisMenu = (arr) => {
+    // filter out tabs that could be awaiting content or need to be removed temporarily
+    const contentTabs = arr.filter(item => item.content !== null);
+
+    const theseTabs = contentTabs.map((item, index) => {
+      let activeMarker = () => {
+        item.onClick( index );
+        this.setState({ activeTab: index + 1 });
+      }
+      return (<Tab label={item.label} onClick={() => activeMarker(index)} key={index} isActive={this.state.activeTab === (index + 1)}/>)
+    });
+    return {theseTabs: theseTabs, contentTabs: contentTabs};
+  }
+
+  panelContent = () => {
+    const thisContent = this.thisMenu(this.props.menuObj).contentTabs[this.state.activeTab - 1].content;
+    return thisContent;
+  }
+
   render() {
     const { menuObj } = this.props
-    const thisMenu = (arr) => {
-      // filter out tabs that could be awaiting content or need to be removed temporarily
-      const contentTabs = arr.filter(item => item.content !== null);
-
-      const theseTabs = contentTabs.map((item, index) => {
-        let activeMarker = () => {
-          item.onClick( index );
-          this.setState({ activeTab: index + 1 });
-        }
-        return (<Tab label={item.label} onClick={() => activeMarker(index)} key={index} isActive={this.state.activeTab === (index + 1)}/>)
-      });
-      return {theseTabs: theseTabs, contentTabs: contentTabs};
-    }
-
-    const panelContent = () => thisMenu(menuObj).contentTabs[this.state.activeTab - 1].content
-
     return (
       <div className="c-tab-container">
-        <TabRender>{thisMenu(menuObj).theseTabs}</TabRender>
-        <div className="c-tab-content">{panelContent()}</div>
+        <TabRender>{this.thisMenu(menuObj).theseTabs}</TabRender>
+        <div className="c-tab-content">{this.panelContent()}</div>
       </div>
     );
   }
